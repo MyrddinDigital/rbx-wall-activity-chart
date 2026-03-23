659e27015ddb0a54190fe12cf639fc8f43f629a6
BluNomad <jon@myrddin.digital>
2026-03-23 16:44:38 -0400

    make gh pages ready + change to dark theme

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const json = require('./data/nfcwall.json');
-const fs = require('fs');
-const https = require('https');
+const json = require("./data/nfcwall.json");
+const fs = require("fs");
+const https = require("https");
 
 // Given raw JSON dump of a wall, extract data representing posts per day.
 function extractData(json) {
   const out = {};
 
-  json.data.forEach(post => {
-    getUserAge(post, userAge => {
+  json.data.forEach((post) => {
+    getUserAge(post, (userAge) => {
       // Ignore accounts less than 2 days old at time of posting.
       if (userAge < 2) return;
-  
+
       const date = new Date(post.created);
       const day = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
-    
+
       if (out[day] >= 0) {
         out[day] = out[day] + 1;
       } else {
@@ -29,36 +29,41 @@ function extractData(json) {
 function getUserAge(post, cb) {
   if (!post.poster) return 0; // banned users
 
-  https.get(`https://users.roblox.com/v1/users/${post.poster.user.userId}`, (res) => {
-    let data = '';
-    res.on('data', (chunk) => {
-      data += chunk
+  https
+    .get(
+      `https://users.roblox.com/v1/users/${post.poster.user.userId}`,
+      (res) => {
+        let data = "";
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        res.on("end", () => {
+          const joinDate = new Date(JSON.parse(data).created);
+          const postDate = new Date(post.created);
+
+          const diffTime = Math.abs(joinDate - postDate);
+          const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+          cb(diffDays);
+        });
+      },
+    )
+    .on("error", (err) => {
+      console.log("Error: " + err.message);
     });
-
-    res.on('end', () => {
-      const joinDate = new Date(JSON.parse(data).created);
-      const postDate = new Date(post.created);
-
-      const diffTime = Math.abs(joinDate - postDate);
-      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-
-      cb(diffDays);
-    });
-  }).on("error", (err) => {
-    console.log("Error: " + err.message);
-  });
 }
 
 // Given a data object of posts per day, write out a CSV file that can be consumed by D3.js
 function writeOut(data) {
-  var stream = fs.createWriteStream("dist/out.csv", {flags:'a'});
+  var stream = fs.createWriteStream("./out.csv", { flags: "a" });
   stream.write(`date,value\n`);
-  
-  Object.entries(data).forEach(entry => {
+
+  Object.entries(data).forEach((entry) => {
     const [date, numPosts] = entry;
     stream.write(`${date},${numPosts}\n`);
   });
-  
+
   stream.end();
 }
 
